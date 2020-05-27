61ab6d6364f7f64d3f3001a0ba580ae1725a969a
leejiwoo2002 <55074799+leejiwoo2002@users.noreply.github.com>
2020-05-27 16:18:24 +0900

    CO2 Chart 기능 추가 시작

--- src/backend/db_select.js
+++ src/backend/db_select.js
@@ -8,7 +8,15 @@ const get_Con_dust = async (dust_size) => {
   }
 };
 
+const getCo2Live = async () => {
+  try {
+    return await axios.get('http://localhost:3000/co2Live'); //dust_size에 따른 url 변경
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 
 module.exports ={
-   get_Con_dust
+  get_Con_dust, getCo2Live
 }
