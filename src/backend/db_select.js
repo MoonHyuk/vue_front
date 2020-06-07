2a0a0142ec28f25ff46177f9345b84ef998e86f3
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-06-08 03:26:34 +0900

    proxy 연결

--- src/backend/db_select.js
+++ src/backend/db_select.js
@@ -1,12 +1,8 @@
 const axios = require('axios');
-const server = "local";
 
 const getCo2Live = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/co2Live'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/co2Live'); //dust_size에 따른 url 변경
+    return await axios.get('/api/co2Live'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -14,10 +10,7 @@ const getCo2Live = async () => {
 
 const getTolueneLive = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/tolueneLive'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/tolueneLive'); //dust_size에 따른 url 변경
+    return await axios.get('/api/tolueneLive'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -25,10 +18,7 @@ const getTolueneLive = async () => {
 
 const getPm1Live = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/pm1Live'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/pm1Live'); //dust_size에 따른 url 변경
+    return await axios.get('/api/pm1Live'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -36,10 +26,7 @@ const getPm1Live = async () => {
 
 const getPm25Live = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/pm25Live'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/pm25Live'); //dust_size에 따른 url 변경
+    return await axios.get('/api/pm25Live'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -47,10 +34,7 @@ const getPm25Live = async () => {
 
 const getPm10Live = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/pm10Live'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/pm10Live'); //dust_size에 따른 url 변경
+    return await axios.get('/api/pm10Live'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -58,10 +42,7 @@ const getPm10Live = async () => {
 
 const getO2Live = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/o2Live'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/o2Live'); //dust_size에 따른 url 변경
+    return await axios.get('/api/o2Live'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
@@ -69,16 +50,13 @@ const getO2Live = async () => {
 
 const getVocLive = async () => {
   try {
-    if(server!=="local")
-      return await axios.get('http://58.121.58.139:3000/vocLive'); //dust_size에 따른 url 변경
-    else
-      return await axios.get('http://localhost:3000/vocLive'); //dust_size에 따른 url 변경
+    return await axios.get('/api/vocLive'); //dust_size에 따른 url 변경
   } catch (error) {
     console.error(error);
   }
 };
 
 
-module.exports ={
+module.exports = {
   getCo2Live, getTolueneLive, getPm1Live, getPm25Live, getPm10Live, getO2Live, getVocLive
 }
